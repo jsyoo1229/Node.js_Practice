e61713e31e64fe9e385008b4286e399553ff9755
Jinsun Yoo <jsyoo1029@gmail.com>
2024-11-11 19:38:56 +0900

    mongodb 연결

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const exp = require('constants')
 const express = require('express')
 const app = express()
 
-app.listen(8080, ()=>{
-    console.log('http://localhost:8080에서 서버 실행중')
-})
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://jsyoo1229:iloveit1229@cluster0.9uogdx4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+new MongoClient(url).connect().then((client)=>{
+    console.log('DB연결성공')
+    db = client.db('forum')
+
+    app.listen(8080, ()=>{
+        console.log('http://localhost:8080에서 서버 실행중')
+    })
+    
+  }).catch((err)=>{
+    console.log(err)
+  })
+
+
+
+
 
 app.get('/news', (req, res)=>{
     res.send('오늘의 뉴스')
